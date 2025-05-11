4a95dc4067aca7d4a4fc5b4498fcd49fbbcf8aeb
Andrew Hedges <andrew@hedges.name>
2025-05-11 13:34:44 -0700

    Implement https://github.com/davidshimjs/qrcodejs/pull/295

--- qrcode.js
+++ qrcode.js
@@ -1422,7 +1422,7 @@ var QRCode;
     var nType = 1;
     var length = _getUTF8Length(sText);
 
-    for (var i = 0, len = QRCodeLimitLength.length; i <= len; i++) {
+    for (var i = 0, len = QRCodeLimitLength.length; i < len; i++) {
       var nLimit = 0;
 
       switch (nCorrectLevel) {
